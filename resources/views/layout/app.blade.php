7f1a6effa691530b280a7698938c46ca550e4589
akali <1131271533@qq.com>
2023-03-18 15:11:59 +0800

    03-18

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=chrome">
-    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="http://121.36.78.64/favicon.ico">
     <title>@yield('title', '神织知更的博客') - 神织知更的博客</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('layout.style')
